804b17c9e2a08387ff3fec44a534557a82e84bb1
İsmail Baran <ismailbaran04@gmail.com>
2025-01-24 23:30:26 +0300

    businnes layer eklendi katmanlar imzalar tamamlandı

---
+++ MyIdentityChatApp.BusinessLayer/Concrete/MessageManager.cs
@@ -0,0 +1,46 @@
+﻿using MyIdentityChatApp.BusinessLayer.Abstract;
+using MyIdentityChatApp.DataAccessLayer.Abstract;
+using MyIdentityChatApp.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIdentityChatApp.BusinessLayer.Concrete
+{
+    public class MessageManager : IMessageService
+    {
+        private readonly IMessageDal _messageDal;
+
+        public MessageManager(IMessageDal messageDal)
+        {
+            _messageDal = messageDal;
+        }
+
+        public void TDelete(int id)
+        {
+            _messageDal.Delete(id);
+        }
+
+        public List<Message> TGetAll()
+        {
+            return _messageDal.GetAll();
+        }
+
+        public Message TGetById(int id)
+        {
+            return _messageDal.GetById(id);
+        }
+
+        public void TInsert(Message entity)
+        {
+            _messageDal.Insert(entity);
+        }
+
+        public void TUpdate(Message entity)
+        {
+            _messageDal.Update(entity);
+        }
+    }
+}
