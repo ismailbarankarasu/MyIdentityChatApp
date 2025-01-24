61fc277301344c90aa6c60173171327e6b0940f3
İsmail Baran <ismailbaran04@gmail.com>
2025-01-24 23:40:29 +0300

    entityler tamamlandı

---
+++ MyIdentityChatApp.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIdentityChatApp.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name{ get; set; }
+        public ICollection<Message> Messages { get; set; }
+    }
+}
---
+++ MyIdentityChatApp.EntityLayer/Concrete/Message.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyIdentityChatApp.EntityLayer.Concrete
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Content { get; set; }
+        public DateTime SentAt { get; set; }
+
+        public int SenderId { get; set; }
+        public ApplicationUser Sender { get; set; }
+
+        public int ReceiverId { get; set; }
+        public ApplicationUser Receiver { get; set; }
+    }
+}
