bdefc1e95b7fa9c4ce4cad581bf87023c7ceb2be
İsmail Baran <ismailbaran04@gmail.com>
2025-01-28 19:24:53 +0300

    default ve login controller oluşturuldu

---
+++ MyIdentityChatApp.PresentationLayer/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyIdentityChatApp.EntityLayer.Concrete;
+using MyIdentityChatApp.PresentationLayer.Models;
+
+namespace MyIdentityChatApp.PresentationLayer.Controllers
+{
+    [Authorize]
+    public class DefaultController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DefaultController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserViewModel model = new UserViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.ImageUrl = values.ImageUrl;
+            return View(model);
+        }
+    }
+}
---
+++ MyIdentityChatApp.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyIdentityChatApp.EntityLayer.Concrete;
+using MyIdentityChatApp.PresentationLayer.Models;
+using System.Runtime.CompilerServices;
+
+namespace MyIdentityChatApp.PresentationLayer.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public LoginController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, true);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
