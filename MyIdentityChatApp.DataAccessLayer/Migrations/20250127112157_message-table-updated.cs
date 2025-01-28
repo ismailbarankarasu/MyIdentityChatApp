2afd0410e6b1d6aa7ed10f085c24e72a1b1ecf21
İsmail Baran <ismailbaran04@gmail.com>
2025-01-28 19:25:15 +0300

    veri tabanı güncellendi

---
+++ MyIdentityChatApp.DataAccessLayer/Migrations/20250127112157_message-table-updated.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyIdentityChatApp.DataAccessLayer.Migrations
+{
+    public partial class messagetableupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Messages",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Messages");
+        }
+    }
+}
