e973884f7025c5d6b1e3f55fe79f965d0a1712ce
İsmail Baran <ismailbaran04@gmail.com>
2025-01-28 19:23:58 +0300

    giriş ve kayıt işllemlerine özel view model oluşturuldu

---
+++ MyIdentityChatApp.PresentationLayer/Models/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyIdentityChatApp.PresentationLayer.Models
+{
+    public class UserViewModel
+    {
+        public string Name{ get; set; }
+        public string Surname{ get; set; }
+        public string ImageUrl{ get; set; }
+    }
+}
