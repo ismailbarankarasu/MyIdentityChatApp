58b1d627718b2ccf30d4a7d93b98c7da0c1498e0
İsmail Baran <ismailbaran04@gmail.com>
2025-02-01 23:50:13 +0300

    sidebar güncellendi

--- MyIdentityChatApp.PresentationLayer/ViewComponents/_AdminLayout/_AdminLayoutSidebarComponentPartial.cs
+++ MyIdentityChatApp.PresentationLayer/ViewComponents/_AdminLayout/_AdminLayoutSidebarComponentPartial.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyIdentityChatApp.EntityLayer.Concrete;
+using MyIdentityChatApp.PresentationLayer.Models;
 
 namespace MyIdentityChatApp.PresentationLayer.ViewComponents._AdminLayout
 {
     public class _AdminLayoutSidebarComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public _AdminLayoutSidebarComponentPartial(UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        public async Task<IViewComponentResult> Invoke()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserViewModel model = new UserViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.ImageUrl = values.ImageUrl;
+            return View(model);
         }
     }
 }
